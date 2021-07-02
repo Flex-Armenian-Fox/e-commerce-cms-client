7d1d66a22e82b761188ae93992268fd27a0b4ab5
anggidast <adastariana@gmail.com>
2021-07-02 21:58:14 +0700

    start make admin dashboard

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span>Dast</span>
+        <span class="font-weight-light">ConceptStore</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed>
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav></Nav>
+    <h1>Homepage</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Nav from "../components/Nav.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Nav }
+};
 </script>
